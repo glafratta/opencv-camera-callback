38c01b032af417f1256666ad3d95fca899f46c7d
glafratta <g.lafratta@outlook.com>
2023-07-19 16:50:24 +0100

    making library camera

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
 	// create the window
 	Window window;
-	window.show();
+	window.showMaximized();
 
 	// call the window.timerEvent function every 40 ms
 	window.startTimer(40);
